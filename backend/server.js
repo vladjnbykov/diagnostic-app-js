82c11fb22bc85f932b27bec796fc20d1054a3fb3
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-06-05 01:45:10 +0200

    test mongoose model

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,47 @@ import bcrypt from 'bcrypt'
 
 // dotenv.config()
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/diagnostisDiabetes"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
+
+const Symptom = mongoose.model('Symptom', {
+    message: String
+})
+
+
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.get('/symptoms', async (req, res) => {
+    const symptoms = await Symptom.find()
+    res.json(symptoms)
+})
+
+app.post('/symptoms', async (req, res) => {
+    const { message } = req.body
+
+    try {
+        const newSymptom = await new Symptom({ message }).save()
+        res.json(newSymptom)
+
+    } catch (error) {
+        res.status(400).json({ message: "Invalid request", error })
+    }
+
+})
+
+
+// Start the server
+app.listen(port, () => {
+    // eslint-disable-next-line
+    console.log(`Server running on http://localhost:${port}`)
+  })
+  
