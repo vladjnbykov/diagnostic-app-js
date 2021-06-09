3f09781f6b647b348a8f36cc6416690aba1e543d
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-06-10 01:06:00 +0200

    age input, radio buttons working, checkbox not working

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+
+import ReactDOM from 'react-dom'
 
 import{ API_URL } from '../reusable/urls'
 
@@ -14,38 +16,150 @@ const Main = () => {
     const dispatch = useDispatch()
     const history = useHistory()
 
+    const [age, setAge] = useState('')
+    const [gender, setGender] = useState('')
+
+    const Checkbox = ({ type = "checkbox", name, checked=false, onChange }) => {
+        console.log("Checkbox: ", name, checked)
+
+        return (
+            <input type={type} name={name} checked={checked} onChange={onChange} />
+
+        )
+    }
+
+
+    const CheckboxGroup = () => {
+        const [checkedItems, setCheckedItems] = useState({})
+
+        const handleChange = (e) => {
+            setCheckedItems({...checkedItems, [e.target.name] : e.target.checked })
+
+        }
+        useEffect(() => {
+            console.log("checkedItems: ", checkedItems)
+        }, [checkedItems])
+
+        const checkboxes = [
+            {
+                name: 'polyuria',
+                key: 'polyuria',
+                label: 'polyuria',
+            },
+            {
+                name: 'polydipsia',
+                key: 'polydipsia',
+                label: 'polydipsia',
+            },
+            {
+                name: 'weakness',
+                key: 'weakness',
+                label: 'weakness',
+            },
+            {
+                name: 'genital-thrush',
+                key: 'genital-thrush',
+                label: 'genital thrush',
+            },
+            {
+                name: 'itching',
+                key: 'itching',
+                label: 'itching',
+            },
+            {
+                name: 'irritability',
+                key: 'irritability',
+                label: 'irritability',
+            },
+            {
+                name: 'delayed-healing',
+                key: 'delayed-healing',
+                label: 'delayed healing',
+            },
+            {
+                name: 'alopecia',
+                key: 'alopecia',
+                label: 'alopecia',
+            },
+            {
+                name: 'obesity',
+                key: 'obesity',
+                label: 'obesity',
+            }
+
+        ]
+    }
+
+
     useEffect(() => {
         if (!accessToken) {
             history.push('/login')
         }
     }, [accessToken, history]) 
     
-    // render all symptoms
-    useEffect(() => { 
-        if(accessToken) {
-            const options = {
-                method: 'GET',
-                headers: {
-                    Authorization: accessToken
-                }
-
-            }
-            fetch(API_URL('symptoms'), options)
-            .then(res => res.json())
-            .then(data => dispatch(symptoms.actions.setSymptoms(data)))
-        }
-    }, [accessToken, dispatch])
-        
-        
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+    }
     
 
     return (
         <div>
             <div>Main</div>
-            {symptomsItems.map(symptom => (
-                <div key={symptom._id}>{symptom.message}</div>
-            ))}
+            
 
+            <form onSubmit={onFormSubmit}>
+                <div>
+                    <p>input your age</p>
+                    <input 
+                        type="number" 
+                        value={age} 
+                        onChange={(e) => setAge(e.target.value)} 
+                />
+
+                </div>
+                <div>
+                    <input
+                        type="radio"
+                        id="male"
+                        name="gender"
+                        value={gender}
+                        onChange={() => setGender('male')}
+                    />
+                    <label htmlFor="male">male</label>
+                    <input
+                        type="radio"
+                        id="female"
+                        name="gender"
+                        value={gender}
+                        onChange={() => setGender('female')}
+                    />
+                    <label htmlFor="female">female</label>
+
+                </div>
+
+                <div>
+                    <label>Checked item name : {checkedItems["polyuria"]}</label> <br/>
+                    {checkboxes.map(item => (
+                            <label key={item.key}>
+                                {item.name}
+                                <Checkbox 
+                                    name={item.name} 
+                                    checked={checkedItems[item.name]} 
+                                    onChange={handleChange} 
+                                />
+
+                            </label>
+                        ))
+                    }
+                </div>
+               
+                
+
+
+
+                <button type="submit">Submit</button>
+
+            </form>
 
         </div>
     )
