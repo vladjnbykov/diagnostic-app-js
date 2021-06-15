d52d6caa3d4f77b0c213a19adf338e4ce10dc201
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-06-15 15:01:10 +0200

    useEffect in prognosis component

--- frontend/src/components/Prognosis.js
+++ frontend/src/components/Prognosis.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+
+
+
 
 const Prognosis = () => {
+    const accessToken = useSelector(store => store.user.accessToken)
 
+    const results = useSelector(store => store.symptoms.risk)
+    const history = useHistory()
+
+    useEffect(() => {
+        if (!accessToken) {
+            history.push('/login')
+        }
+    }, [accessToken, history])
+    
     return (
         <div>
-            PROGNOSIS
+            {results}
+            {console.log(results)}
         </div>
     )
     
