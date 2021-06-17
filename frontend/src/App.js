72b2549a557173717f984b3725659c4910828c7c
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-06-17 12:29:56 +0200

    warning on styled component in Prognosis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Login from "./components/Login";
 
-import Prognosis from "./components/Prognosis";
+//import Prognosis from "./components/Prognosis";
 
 import Header from "./components/Header";
 import user from "./reducers/user";
@@ -27,7 +27,6 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route path="/login" component={Login} />
-          <Route path="/prognosis" component={Prognosis} />
         </Switch>
       </Provider>
     </BrowserRouter>
--- frontend/src/components/Prognosis.js
+++ frontend/src/components/Prognosis.js
@@ -1,74 +1,77 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
-import { useSelector } from "react-redux"
-import { useHistory } from "react-router-dom"
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-import styled from 'styled-components'
-import './prognosis.css'
+import styled from "styled-components";
 
+import "./prognosis.css";
 
+//
+{/*const Background = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px;
+    background-color: ${props => props.color};
+    border: 1px solid navy;
+    margin: 50px 40px 50px 40px;
+  `;
+*/}
+
+  //
 const Prognosis = () => {
-  const accessToken = useSelector((store) => store.user.accessToken)
+  const accessToken = useSelector((store) => store.user.accessToken);
 
-  const results = Math.round(useSelector((store) => store.symptoms.risk))
-  const history = useHistory()
+  const results = Math.round(useSelector((store) => store.symptoms.risk));
+  const history = useHistory();
 
- 
+  
 
   const isBackgroundColor = () => {
-    let color
+    let color;
     if (results >= 0 && results < 15) {
-      color = "#CCCCFF"
+      color = "#CCCCFF";
     } else if (results >= 15 && results < 40) {
-      color = "#D0FFCD"
+      color = "#D0FFCD";
     } else if (results >= 40 && results < 70) {
-      color = "#FFFFCD"
+      color = "#FFFFCD";
     } else if (results >= 70) {
-      color = "#FFCDCD"
+      color = "#FFCDCD";
     }
-    return color
-  }
+    // return <Background color={props.color} />
+    return (color)
+  };
 
   const Background = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 5px;
-  background-color: ${isBackgroundColor()};
-  border: 1px solid navy;
-  margin: 50px 40px 50px 40px;
-  `    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px;
+    background-color: ${isBackgroundColor()};
+    border: 1px solid navy;
+    margin: 50px 40px 50px 40px;
+  `;
+
 
 
 
   useEffect(() => {
     if (!accessToken) {
-      history.push("/login")
+      history.push("/login");
     }
-  }, [accessToken, history])
+  }, [accessToken, history]);
 
   return (
     <>
       <div className="prognosis">
         <h2>Results</h2>
         <Background>
-          <h5>
-            Your risk of developing diabetes is {results}%
-          </h5>
-
+          <h5>Your risk of developing diabetes is {results}%</h5>
         </Background>
-      
-        
-
       </div>
-
-    
     </>
-
-
-    
-    
-  )
+  );
 };
 
-export default Prognosis
+export default Prognosis;
