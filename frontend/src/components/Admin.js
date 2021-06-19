e02317cdf6eb0ec1ca47e2b38e79a90d09fd4f4a
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-06-19 23:43:43 +0200

    symptoms fixed, made footer sticky

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -3,14 +3,14 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
+import "./admin.css";
 import "./prognosis.css";
 
 const Admin = () => {
   const accessToken = useSelector((store) => store.user.accessToken);
 
-  
   const listItems = useSelector((store) => store.symptoms.items);
-  
+
   const history = useHistory();
 
   useEffect(() => {
@@ -21,32 +21,26 @@ const Admin = () => {
 
   return (
     <div>
-      SYMPTOMS
+      <h2 className="patient-title">Patients</h2>
       {console.log("list", listItems)}
       {console.log("individ", Object.entries(listItems[0].checkedItems))}
 
       {listItems.map((item) => (
         <div className="patients" key={item._id}>
-          {item.username}
-          {item.age}
-          {item.gender}
+          <div>username: {item.username}</div>
+          <div>age: {item.age}</div>
+          <div>gender: {item.gender}</div>
 
-
-          
-          {Object.entries(item.checkedItems).map(item => Object.values(String(item) ))}
-            
-            
-
-
+          <div>
+            noted:{" "}
+            {Object.entries(item.checkedItems).map((item) => (
+              <div>{item}</div>
+            ))}
+          </div>
         </div>
       ))}
-
     </div>
   );
 };
 
 export default Admin;
-
-{/*
-{Object.entries(item.checkedItems).map(item => item)}
-*/}
