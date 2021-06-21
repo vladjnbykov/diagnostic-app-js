7a918f817fc2c2775f8f04eb14e91420948bc39d
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-06-21 22:17:44 +0200

    exp with url

--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -1,4 +1,6 @@
-const BASE_URL = 'http://localhost:8080'
+// const BASE_URL = 'http://localhost:8080'
+
+const BASE_URL = 'https://vlad-diagnostic-diabetes-front.herokuapp.com'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}` 
 
