5fa18fd668b5a045315caa44abfea3af068ba039
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-06-21 15:52:06 +0200

    connecting to Atlas Mongo DB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 /* eslint-disable linebreak-style */
-// import dotenv from 'dotenv'
+import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import crypto from 'crypto'
 import bcrypt from 'bcrypt'
 
-// dotenv.config()
+dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/diagnostisDiabetes"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
